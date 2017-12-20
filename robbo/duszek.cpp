9554826018743a883ac9a1724b3decb1f2668446
Tetiuz <lunarsmoons@gmail.com>
2017-12-20 01:31:52 +0100

    Drzwi + Kluczyki

--- robbo/duszek.cpp
+++ robbo/duszek.cpp
@@ -1,7 +1,25 @@
 #include "duszek.h"
 duszek::duszek(int x,int y) : obiekt(x,y,'D')
 {
-	
-		
+/*	switch( rand() % 4 ) {
+ 
+            case 0: 
+                // go up
+                cout << "up" << endl;
+                break;
+            case 1:
+                // go down
+                cout << "down" << endl;
+                break;
+            case 2:
+                // go left
+                cout << "left" << endl;
+                break;
+            default:
+                // go right
+                cout << "right" << endl;
+                break;
+               }    
+*/		
 }
 
